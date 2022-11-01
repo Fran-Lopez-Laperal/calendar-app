ceafca3307f590fe95ba66c6a24f454ddd8e644f
Fran <fransanxenxo@gmail.com>
2022-11-01 12:16:44 +0000

    feat: create controller user login

--- backend-calendar/controllers/auth.js
+++ backend-calendar/controllers/auth.js
@@ -1,5 +1,5 @@
 
-const { response } = require('express');
+const { response, json } = require('express');
 const bcrypt = require('bcryptjs')
 const User = require('../models/user.model')
 
@@ -22,6 +22,8 @@ module.exports.crearUsuario = async (req, res = response, next) => {
         user = new User(req.body)
 
         //Encriptar constraseña con librería bcryptjs
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt)
 
         await user.save()
 
@@ -46,16 +48,45 @@ module.exports.crearUsuario = async (req, res = response, next) => {
 }
 
 
-module.exports.loginUsuario = (req, res = response, next) => {
+module.exports.loginUsuario = async(req, res = response, next) => {
 
     const { email, password } = req.body;
 
-    res.status(201).json({
-        ok: true,
-        msg: 'login',
-        password,
-        email
-    })
+    try {
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            res.status(400).json({
+                ok: false,
+                msg: 'El usuario no existe con ese email'
+            })
+        }
+
+        //Confirmar los password
+        const validPassword = bcrypt.compareSync(password, user.password)
+
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password incorrecto'
+            })
+        }
+
+        //Generar nuesto JWT Json Web Token
+        res.json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        })
+
+
+    } catch {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        })
+    }
 };
 
 module.exports.revalidarToken = (req, res = response, next) => {
